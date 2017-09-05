5b9640af62b47e9d138da397d9393b8f7b4c5afe
sandydcr <sandy86sr@gmail.com>
2017-09-05 15:24:05 +0530

    checking git

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 'use strict';
-console.log('sandy');
+const express = require('express');
+const app = express();
+
+app.set('port',process.env.PORT || 3001);
+app.set('view engine','ejs');
+//uses stream to prcocess al the file request itslef a server instance doesnot come up with the application instance
+//hence needed to declared explicilty
+app.use(express.static('public'));
+
+app.get('/',function(req, res,next){
+  //res.send('hello express');
+  //res.sendFile(__dirname +'/view/login.htm');
+  res.render('login');
+})
+
+app.listen(app.get('port'),function(){
+  console.log('server running on port', app.get('port'));
+})
